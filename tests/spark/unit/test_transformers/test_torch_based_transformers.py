c977d90d899f55e43aaded5aec0f9470fd8dbbdb
fonhorst <fonhorst@alipoov.nb@gmail.com>
2021-11-11 21:03:14 +0300

    ---

--- tests/spark/unit/test_transformers/test_torch_based_transformers.py
+++ tests/spark/unit/test_transformers/test_torch_based_transformers.py
@@ -3,11 +3,14 @@ import torch
 from pyspark.sql import SparkSession
 
 import pandas as pd
+import numpy as np
 
 from lightautoml.dataset.np_pd_dataset import PandasDataset
 from lightautoml.dataset.roles import TextRole, PathRole
 from lightautoml.image.utils import pil_loader
+from lightautoml.spark.dataset.roles import NumericVectorOrArrayRole
 from lightautoml.spark.transformers.image import PathBasedAutoCVWrap as SparkPathBasedAutoCVWrap
+from lightautoml.spark.transformers.image import ArrayBasedAutoCVWrap as SparkArrayBasedAutoCVWrap
 from .test_transformers import smoke_check
 
 
@@ -22,7 +25,7 @@ def spark() -> SparkSession:
     spark.stop()
 
 
-def test_auto_cv_wrap(spark: SparkSession):
+def test_path_auto_cv_wrap(spark: SparkSession):
     source_data = pd.DataFrame(data={
         "path_a": [f"resources/images/cat_{i + 1}.jpg" for i in range(3)],
         "path_b": [f"resources/images/cat_{i + 1}.jpg" for i in range(3)]
@@ -32,5 +35,17 @@ def test_auto_cv_wrap(spark: SparkSession):
 
     result_ds = smoke_check(spark, ds, SparkPathBasedAutoCVWrap(image_loader=pil_loader, device=torch.device("cpu:0")))
 
+    assert result_ds.shape == ds.shape
+
+
+# def test_array_auto_cv_wrap(spark: SparkSession):
+#     source_data = pd.DataFrame(data={
+#         "path_a": [np.array(pil_loader(f"resources/images/cat_{i + 1}.jpg")) for i in range(3)],
+#         "path_b": [np.array(pil_loader(f"resources/images/cat_{i + 1}.jpg")) for i in range(3)]
+#     })
+#
+#     ds = PandasDataset(source_data, roles={name: NumericVectorOrArrayRole(None, None, is_vector=False) for name in source_data.columns})
+#
+#     result_ds = smoke_check(spark, ds, SparkArrayBasedAutoCVWrap(device=torch.device("cpu:0")))
 
 
