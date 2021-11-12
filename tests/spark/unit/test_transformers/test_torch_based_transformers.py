4f92fb9f906794c72461cc807cca7b243d10a4bf
fonhorst <fonhorst@alipoov.nb@gmail.com>
2021-11-12 11:55:56 +0300

    added test for AutoCVWrap

--- tests/spark/unit/test_transformers/test_torch_based_transformers.py
+++ tests/spark/unit/test_transformers/test_torch_based_transformers.py
@@ -1,20 +1,30 @@
+import numpy as np
 import pandas as pd
 import pytest
 import torch
-
-import numpy as np
-
 from pyspark.sql import SparkSession
 
 from lightautoml.dataset.np_pd_dataset import PandasDataset
 from lightautoml.dataset.roles import PathRole, NumericRole
 from lightautoml.image.utils import pil_loader
-from lightautoml.spark.dataset.roles import NumericVectorOrArrayRole
 from lightautoml.spark.transformers.image import PathBasedAutoCVWrap as SparkPathBasedAutoCVWrap, \
     ImageFeaturesTransformer as SparkImageFeaturesTransformer
+from lightautoml.transformers.text import AutoNLPWrap
+from lightautoml.spark.transformers.text import AutoNLPWrap as SparkAutoNLPWrap
 from lightautoml.transformers.image import ImageFeaturesTransformer
 from . import compare_by_content
 from .test_transformers import smoke_check
+
+
+@pytest.fixture(scope="session")
+def spark() -> SparkSession:
+    spark = SparkSession.builder.config("master", "local[1]").getOrCreate()
+
+    print(f"Spark WebUI url: {spark.sparkContext.uiWebUrl}")
+
+    yield spark
+
+    spark.stop()
 
 
 @pytest.fixture
@@ -28,19 +38,22 @@ def image_dataset() -> PandasDataset:
 
     return ds
 
-@pytest.fixture(scope="session")
-def spark() -> SparkSession:
-    spark = SparkSession.builder.config("master", "local[1]").getOrCreate()
 
-    print(f"Spark WebUI url: {spark.sparkContext.uiWebUrl}")
+@pytest.fixture
+def text_dataset() -> PandasDataset:
+    source_data = pd.DataFrame(data={
+        "text_a": [f"Lorem Ipsum is simply dummy text of the printing and typesetting industry. {i}" for i in range(3)],
+        "text_b": [f"Lorem Ipsum is simply dummy text of the printing and typesetting industry. {i}" for i in range(3)]
+    })
 
-    yield spark
+    ds = PandasDataset(source_data, roles={name: PathRole() for name in source_data.columns})
 
-    spark.stop()
+    return ds
 
 
 def test_path_auto_cv_wrap(spark: SparkSession, image_dataset: PandasDataset):
-    result_ds = smoke_check(spark, image_dataset, SparkPathBasedAutoCVWrap(image_loader=pil_loader, device=torch.device("cpu:0")))
+    result_ds = smoke_check(spark, image_dataset, SparkPathBasedAutoCVWrap(image_loader=pil_loader,
+                                                                           device=torch.device("cpu:0")))
 
     # TODO: replace with a normal content check
     assert result_ds.shape[0] == image_dataset.shape[0]
@@ -51,3 +64,16 @@ def test_image_features_transformer(spark: SparkSession, image_dataset: PandasDa
     compare_by_content(spark, image_dataset,
                        ImageFeaturesTransformer(n_jobs=1, loader=pil_loader),
                        SparkImageFeaturesTransformer(n_jobs=1, loader=pil_loader))
+
+
+@pytest.mark.skip
+def test_auto_nlp_wrap(spark: SparkSession, text_dataset: PandasDataset):
+    kwargs = {
+        "model_name": "random_lstm",
+        "device": torch.device("cpu:0"),
+        "embedding_model": None
+    }
+    compare_by_content(spark, text_dataset,
+                       AutoNLPWrap(**kwargs),
+                       SparkAutoNLPWrap(**kwargs))
+
