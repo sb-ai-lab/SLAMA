e546d64d5cc3af0c68e2776fa434c052c0ce73d6
fonhorst <fonhorst@alipoov.nb@gmail.com>
2022-10-19 17:35:23 +0300

    added publishing settings for scala-based artefacts

--- examples/spark/spark-ml-pipe-lgb.py
+++ examples/spark/spark-ml-pipe-lgb.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     spark = get_spark_session()
 
     seed = 42
-    cv = 5
+    cv = 2
     dataset_name = "lama_test_dataset"
     path, task_type, roles, dtype = get_dataset_attrs(dataset_name)
 
--- scala-lightautoml-transformers/project/plugins.sbt
+++ scala-lightautoml-transformers/project/plugins.sbt
@@ -1,2 +1,3 @@
 //addSbtPlugin("org.jetbrains" % "sbt-ide-settings" % "1.1.0")
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "1.2.0")
+addSbtPlugin("com.github.sbt" % "sbt-pgp" % "2.1.2")
