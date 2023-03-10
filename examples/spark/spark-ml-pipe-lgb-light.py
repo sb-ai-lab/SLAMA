Distributed/fix/averaging transformer (#175)
* add ability to pass external columns untouched through the whole transformer pipeline generated by SLAMA
---------

Co-authored-by: fonhorst <fonhorst@alipoov.nb@gmail.com>

examples/spark/spark-ml-pipe-lgb-light.py
@@ -2,8 +2,9 @@ import logging.config
 import logging.config
 
 from pyspark.ml import PipelineModel
-from pyspark.sql import functions as F
+from pyspark.sql import functions as sf
 
+from examples.spark.examples_utils import check_columns
 from examples_utils import get_persistence_manager
 from examples_utils import get_spark_session, prepare_test_and_train, get_dataset_attrs
 from sparklightautoml.dataset.base import SparkDataset
@@ -50,7 +51,7 @@ if __name__ == "__main__":
         sreader = SparkToSparkReader(task=task, cv=cv, advanced_roles=False)
         sdataset = sreader.fit_read(train_df, roles=roles, persistence_manager=persistence_manager)
 
-        iterator = SparkFoldsIterator(sdataset)#.convert_to_holdout_iterator()
+        iterator = SparkFoldsIterator(sdataset).convert_to_holdout_iterator()
 
         spark_ml_algo = SparkBoostLGBM(freeze_defaults=False, use_single_dataset_mode=False)
         spark_features_pipeline = SparkLGBSimpleFeatures()
@@ -66,6 +67,9 @@ if __name__ == "__main__":
         oof_score = score(oof_preds_ds[:, spark_ml_algo.prediction_feature])
         logger.info(f"OOF score: {oof_score}")
 
+        test_column = "some_external_column"
+        test_df = test_df.withColumn(test_column, sf.lit(42.0))
+
         # 1. first way (LAMA API)
         test_sds = sreader.read(test_df, add_array_attrs=True)
         test_preds_ds = ml_pipe.predict(test_sds)
@@ -75,17 +79,21 @@ if __name__ == "__main__":
 
         # 2. second way (Spark ML API, save-load-predict)
         transformer = PipelineModel(stages=[sreader.transformer(add_array_attrs=True), ml_pipe.transformer()])
+
         transformer.write().overwrite().save("/tmp/reader_and_spark_ml_pipe_lgb")
 
         pipeline_model = PipelineModel.load("/tmp/reader_and_spark_ml_pipe_lgb")
         test_pred_df = pipeline_model.transform(test_df)
+
+        check_columns(test_df, test_pred_df)
+
         test_pred_df = test_pred_df.select(
             SparkDataset.ID_COLUMN,
-            F.col(roles['target']).alias('target'),
-            F.col(spark_ml_algo.prediction_feature).alias('prediction')
+            sf.col(roles['target']).alias('target'),
+            sf.col(spark_ml_algo.prediction_feature).alias('prediction')
         )
         test_score = score(test_pred_df)
-        logger.info(f"Test score (#3 way): {test_score}")
+        logger.info(f"Test score (#2 way): {test_score}")
 
     logger.info("Finished")
 